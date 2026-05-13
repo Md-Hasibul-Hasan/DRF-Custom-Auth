8a056782be5988095536f538720e8187b8715145
Md Hasibul Hasan <hasib.hasan.th@gmail.com>
2026-05-13 16:55:13 +0600

    migrate db to postgres

--- DRF_Auth/settings.py
+++ DRF_Auth/settings.py
@@ -119,12 +119,29 @@ WSGI_APPLICATION = 'DRF_Auth.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv('DB_NAME', 'drf_custom_auth'),
+        "USER": os.getenv('DB_USER', 'postgres'),
+        "PASSWORD": os.getenv('DB_PASSWORD', ''),
+        "HOST": os.getenv('DB_HOST', 'localhost'),
+        "PORT": os.getenv('DB_PORT', '5432'),
     }
 }
+
+DB_SCHEMA = os.getenv('DB_SCHEMA')
+if DB_SCHEMA:
+    DATABASES['default']['OPTIONS'] = {
+        'options': f'-c search_path={DB_SCHEMA}'
+    }
 
 
 # Password validation
